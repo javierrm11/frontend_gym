7361f0f73d94a037bf21d56d402fc84f29f4cb75
Javier <javierrumo2@gmail.com>
2025-09-14 15:54:18 +0200

    h

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <div id="app">
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,viewport-fit=cover">
+    <title>Document</title>
+  </head>
+  <body>
+    <div id="app">
     <Header />
     <router-view />
     <Footer />
-  </div>
+    </div>
+  </body>
+  </html>
 </template>
 
 <script>
